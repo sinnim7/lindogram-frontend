1ef82c34ac00f239b7f15e43fcc18a6374be7d89
lindolindolindo <sinnim7@gmail.com>
2020-01-04 09:54:43 +0900

    frontend setup

---
+++ src/Components/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { gql } from "apollo-boost";
+
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
+import Router from "./Router";
+import { useQuery } from "react-apollo-hooks";
+
+//@client => query가 api 가는 거 방지.
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+export default () => {
+  const {
+    data: { isLoggedIn }
+  } = useQuery(QUERY);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <>
+        <GlobalStyles />
+        <Router isLoggedIn={isLoggedIn} />
+      </>
+    </ThemeProvider>
+  );
+};
