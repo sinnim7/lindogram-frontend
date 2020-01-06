8da3f3586cdca8be8ee727c37780d77fb112d28f
lindolindolindo <sinnim7@gmail.com>
2020-01-06 18:23:48 +0900

    add style

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { gql } from "apollo-boost";
-
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
 import { useQuery } from "react-apollo-hooks";
+import Footer from "./Footer";
 
 //@client => query가 api 가는 거 방지.
 const QUERY = gql`
   {
     isLoggedIn @client
   }
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 935px;
+  width: 100%;
 `;
 
 export default () => {
@@ -21,10 +27,11 @@ export default () => {
 
   return (
     <ThemeProvider theme={Theme}>
-      <>
+      <Wrapper>
         <GlobalStyles />
         <Router isLoggedIn={isLoggedIn} />
-      </>
+        <Footer />
+      </Wrapper>
     </ThemeProvider>
   );
 };
--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,1 +1,22 @@
-export default () => "Auth";
+import React, { useState } from "react";
+import styled from "styled-components";
+const Wrapper = styled.div`
+  height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Box = styled.div`
+  ${props => props.theme.whiteBox}
+`;
+
+export default () => {
+  const [action] = useState("logIn");
+
+  return (
+    <Wrapper>
+      {action === "logIn" ? <Box>Log In</Box> : <Box>Sign Up</Box>}
+    </Wrapper>
+  );
+};
